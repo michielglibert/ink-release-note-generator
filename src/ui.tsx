c847cee01fc8c22151a7fcd2c9a96c05cdd18df9
Michiel <michiel@wegroup.be>
2021-10-08 11:43:00 +0200

    Adjust main file

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,100 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, Box, useInput } from "ink";
 import { exit } from "process";
+import { ReleaseNote, ReleaseNoteType } from "./types";
+import ReleaseNoteTypeView from "./components/ReleaseNoteTypeView";
 
-interface MenuOptionProps {
-	isActive: boolean;
+enum View {
+	ReleaseNoteType,
+	Project,
+	Message,
 }
 
-const MenuOption: React.FC<MenuOptionProps> = ({ isActive, children }) => {
+const App = () => {
+	const [releaseNote, setReleaseNote] = useState<ReleaseNote>({
+		type: undefined,
+		project: undefined,
+		message: undefined,
+	});
+
+	const [currentView, setCurrentView] = useState<View>(0);
+
+	useInput((input) => {
+		if (input === "q") exit();
+	});
+
+	const handleReleaseNoteTypeChange = (value: ReleaseNoteType) => {
+		setReleaseNote({ ...releaseNote, type: value });
+		setCurrentView(View.Project);
+	};
+
 	return (
 		<Box>
-			<Text color="yellow">{isActive && "> "}</Text>
-			<Text color="red">{children}</Text>
+			{{
+				[View.ReleaseNoteType]: (
+					<ReleaseNoteTypeView handleChange={handleReleaseNoteTypeChange} />
+				),
+				[View.Project]: <></>,
+				[View.Message]: <></>,
+			}[currentView] || <Text>View not defined</Text>}
 		</Box>
 	);
 };
 
-const App = () => {
-	const releaseTypeOptions = ["Feature", "Improvement", "Fix"];
-	const [activeOption, setActiveOption] = React.useState(0);
-
-	const handleNextOption = () => {
-		if (activeOption === releaseTypeOptions.length - 1) setActiveOption(0);
-		else setActiveOption((activeOption) => activeOption + 1);
-	};
-
-	const handlePreviousOption = () => {
-		if (activeOption === 0) setActiveOption(2);
-		else setActiveOption((activeOption) => activeOption - 1);
-	};
-
-	useInput((input, key) => {
-		if (input === "q") exit();
-
-		if (key.downArrow) handleNextOption();
-		if (key.upArrow) handlePreviousOption();
-	});
-
-	return (
-		<Box flexDirection="column">
-			<Box>
-				<Box borderStyle="single" marginX={2} marginY={1} paddingX={2}>
-					<Text color="green">ADD A CHANGELOG</Text>
-				</Box>
-			</Box>
-			<Text color="green">What type of releasenote do you want to add?</Text>
-			{releaseTypeOptions.map((releaseType, index) => (
-				<MenuOption
-					key={`MenuOption_${releaseType}`}
-					isActive={index === activeOption}
-				>
-					{releaseType}
-				</MenuOption>
-			))}
-		</Box>
-	);
-};
-
-const chooseReleaseNote = () => {
-	const releaseTypeOptions = ["Feature", "Improvement", "Fix"];
-	const [activeOption, setActiveOption] = React.useState(0);
-
-	const handleNextOption = () => {
-		if (activeOption === releaseTypeOptions.length - 1) setActiveOption(0);
-		else setActiveOption((activeOption) => activeOption + 1);
-	};
-
-	const handlePreviousOption = () => {
-		if (activeOption === 0) setActiveOption(2);
-		else setActiveOption((activeOption) => activeOption - 1);
-	};
-
-	useInput((input, key) => {
-		if (input === "q") exit();
-
-		if (key.downArrow) handleNextOption();
-		if (key.upArrow) handlePreviousOption();
-	});
-
-	return (
-		<Box flexDirection="column">
-			<Box>
-				<Box borderStyle="single" marginX={2} marginY={1} paddingX={2}>
-					<Text color="green">ADD A CHANGELOG</Text>
-				</Box>
-			</Box>
-			<Text color="green">What type of releasenote do you want to add?</Text>
-			{releaseTypeOptions.map((releaseType, index) => (
-				<MenuOption
-					key={`MenuOption_${releaseType}`}
-					isActive={index === activeOption}
-				>
-					{releaseType}
-				</MenuOption>
-			))}
-		</Box>
-	);
-};
-
-module.exports = App;
+export default App;
